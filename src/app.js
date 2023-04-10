4fc6d0a1c35dbbc8559d5c536399598d89ddb8c3
Daniel Dias <daniiel2dias@gmail.com>
2023-04-10 08:49:23 -0300

    post tweets

--- src/app.js
+++ src/app.js
@@ -24,4 +24,23 @@ app.post("/sign-up", (req, res) => {
     res.status(201).send(req.body);
 });
 
+app.post("/tweets", (req, res) => {
+    const { tweet } = req.body;
+    const username = req.body.username;
+  
+    const isUserSigned = users.find((u) => u.username === username);
+    if (!isUserSigned) {
+      return res.status(401).send("UNAUTHORIZED");
+    }
+  
+    const usernameString = typeof username === "string";
+    const tweetString = typeof tweet === "string";
+    if (!username || !tweet || !tweetString || !usernameString) {
+      return res.status(400).send("Todos os campos são obrigatórios!");
+    }
+  
+    tweets.unshift({ username, tweet });
+    res.status(201).send("OK!");
+});
+
 app.listen(5000, () => console.log("server online"));
