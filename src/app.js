473a9287a66010de7f5246c178b66cfca30a05a9
Daniel Dias <daniiel2dias@gmail.com>
2023-06-24 08:26:17 -0300

    bugfix rota post/tweets

--- src/app.js
+++ src/app.js
@@ -12,39 +12,41 @@ const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const { avatar, username } = req.body;
-    const { user } = req.headers;
+    
 
-    const usernameString = typeof username === "string";
-    const avatarString = typeof avatar === "string";
+    const usernameIsString = typeof username === "string";
+    const avatarIsString = typeof avatar === "string";
 
-    if ( !username || !avatar || !usernameString || !avatarString ) {
+    if ( !username || !avatar || !usernameIsString || !avatarIsString ) {
     return res.status(400).send("Todos os campos são obrigatórios!");
     }
 
-    if(users.find(({username}) => username === user)) {
+    if(users.find(({username}) => username === username)) {
       return res.status(409).send("Nome de usuário já em uso, por favor, escolha outro!")
     }
 
 
-    users.push({username: user, avatar});
+    users.push({username, avatar});
     res.status(201).send(req.body);
 });
 
 app.post("/tweets", (req, res) => {
-    const { tweet, username } = req.body;
+    const { tweet } = req.body;
+    const { user } = req.headers;
   
-    const isUserSigned = users.find((u) => u.username === username);
-    if (!isUserSigned) {
+    const signedUp = users.find((u) => u.username === user);
+
+    if (!signedUp) {
       return res.status(401).send("UNAUTHORIZED");
     }
   
-    const usernameString = typeof username === "string";
-    const tweetString = typeof tweet === "string";
-    if (!username || !tweet || !tweetString || !usernameString) {
+    const usernameIsString = typeof user === "string";
+    const tweetIsString = typeof tweet === "string";
+    if (!user || !tweet || !tweetIsString || !usernameIsString) {
       return res.status(400).send("Todos os campos são obrigatórios!");
     }
-    let avatar = users.find((u)=> u.username === username)
-    tweets.unshift({ username, tweet, avatar: avatar.avatar });
+    let avatar = users.find((u)=> u.username === user)
+    tweets.unshift({ user, tweet, avatar: avatar.avatar });
     res.status(201).send("OK!");
 });
 
